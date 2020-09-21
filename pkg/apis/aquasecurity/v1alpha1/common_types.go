c14ef976d6633de35b494bc70dde2ba253816b06
Daniel Sagi <daniel@example.com>
2020-09-21 08:01:43 -0700

    moved declaration of generatedat struct to common types

--- pkg/apis/aquasecurity/v1alpha1/common_types.go
+++ pkg/apis/aquasecurity/v1alpha1/common_types.go
@@ -1,8 +1,14 @@
 package v1alpha1
 
+import "time"
+
 // Scanner is the spec for a scanner generating a security assessment report.
 type Scanner struct {
 	Name    string `json:"name"`
 	Vendor  string `json:"vendor"`
 	Version string `json:"version"`
+}
+
+type GeneratedAt struct {
+	Time time.Time `json:"timestamp"`
 }
